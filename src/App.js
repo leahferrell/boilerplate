6659e2a9b0f4f6527d695a5bc71d82b5de1dc3af
Leah Ferrell <leahferrell03@gmail.com>
2020-04-26 13:23:00 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Layout from "./layout/Layout";
+import ComponentsPage from "./pages/ComponentsPage";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import ArticlePage from "./pages/ArticlePage";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import AboutPage from "./pages/AboutPage";
+import ContactPage from "./pages/ContactPage";
+import WidgetsPage from "./pages/WidgetsPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+		<Layout>
+			<BrowserRouter>
+				<Switch>
+					<Route exact path = "/" component = {HomePage}/>
+					<Route path = "/articles" component = {ArticlePage} />
+					<Route path = "/components" component = {ComponentsPage} />
+					<Route path= "/widgets" component={WidgetsPage} />
+					<Route path= "/about" component={AboutPage}/>
+					<Route path="/contact" component={ContactPage}/>
+					<Route component={NotFoundPage}/>
+				</Switch>
+			</BrowserRouter>
+		</Layout>
   );
 }
 
